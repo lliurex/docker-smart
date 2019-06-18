6f3ffb71fcc1def1417c354c0a7874b72dbdafad
M.Angel Juan <m.angel.juan@gmail.com>
2019-06-18 16:54:23 +0200

    Fix build shell

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gbp buildpackage -us -uc -rfakeroot --git-export-dir=../build-area --git-debian-branch=master --git-cleaner=fakeroot debian/rules clean
+gbp buildpackage -us -uc -rfakeroot --git-export-dir=../build-area --git-debian-branch=debian/bionic --git-cleaner=fakeroot debian/rules clean
