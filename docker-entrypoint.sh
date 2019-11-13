b96d25dbd508118e2f8ae60b6056ad9a45235e42
M.Angel Juan <m.angel.juan@gmail.com>
2019-11-13 13:27:54 +0100

    Fix 100% cpu bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,7 @@ if [ -z "$GROUP" -o -z "$GID" ]; then
 fi
 SERVICES="/bin/dbus-uuidgen --ensure;/bin/dbus-daemon --system --fork;/usr/bin/pulseaudio --system --daemonize --high-priority --log-target=syslog --disallow-exit --disallow-module-loading=1"
 #rm -rf /tmp/*
-bash /usr/sbin/nwfermi_daemon.sh
+screen -d -m bash /usr/sbin/nwfermi_daemon.sh
 rm -f /var/run/dbus/pid > /dev/null 2>&1
 sleep 1
 IFS=';'
@@ -25,7 +25,7 @@ if [ ! -L '/root/lliurex-smart-storage' ]; then
     ln -s /home/${USER} /root/lliurex-smart-storage || true
 fi
 export LANG=es_ES.UTF-8
-/opt/SMART\ Technologies/SMART\ Product\ Drivers/bin/.SMARTBoardService_elf &
+screen -d -m /opt/SMART\ Technologies/SMART\ Product\ Drivers/bin/.SMARTBoardService_elf &
 /opt/SMART\ Technologies/SMART\ Product\ Drivers/bin/.SMART\ Board\ Tools_elf
 chown -R $USER:$GROUP /home/${USER} || true
 
