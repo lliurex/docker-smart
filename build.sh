ecdec9e8ebe26110b78ec9b480468a5103a342b6
M.Angel Juan <m.angel.juan@gmail.com>
2020-12-14 14:21:49 +0100

    Merge experimental changes

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-gbp buildpackage -us -uc -rfakeroot --git-export-dir=../build-area --git-debian-branch=debian/bionic --git-cleaner='fakeroot debian/rules clean'
+current_branch="$(git branch 2>/dev/null |sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
+gbp buildpackage -ai386 --build=any -us -uc -rfakeroot --git-export-dir=../build-area --git-debian-branch=$current_branch --git-cleaner='fakeroot debian/rules clean'
+gbp buildpackage -us -uc -rfakeroot --git-export-dir=../build-area --git-debian-branch=$current_branch --git-cleaner='fakeroot debian/rules clean'
